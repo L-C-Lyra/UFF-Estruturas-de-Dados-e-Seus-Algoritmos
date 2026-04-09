0559f2dd9cec227fa6d08222039c86137527da95
Lucas C Lyra Pereira <148347259+L-C-Lyra@users.noreply.github.com>
2026-04-09 10:29:50 -0300

    :sparkles: feat: "Trabalhos de Implementacao/01 - Arvore Zigue-Zague"

---
+++ Trabalhos de Implementacao/01 - Arvore Zigue-Zague/README.md
@@ -0,0 +1,25 @@
+[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/lS9To3iE)
+#### Problema 
+
+Implemente a função int ehZigueZague(TNoA *raiz)  que recebe um ponteiro para a raiz de uma árvore binária de busca e retorna um inteiro (1 se a árvore é zigue-zague, 0 caso contrário). 
+
+Use o arquivo fornecido nesse exercício, pois ele já contém o tratamento de entrada e saída. 
+
+#### Entrada: 
+- Árvore binária de busca a ser analisada. Os nós da árvore devem ser informados separados por traço, na ordem em que devem ser inseridos na árvore (o esqueleto fornecido já realiza a inserção). Os valores dos nós devem ser informados separados por um traço, sem espaço em branco entre o valor do nó e o traço.
+
+#### Saída:
+- inteiro (1 se a árvore é zigue-zague, 0 caso contrário)
+
+#### Exemplo1:
+
+|Entrada|Saída|
+|---|---| 
+|100-200-20|0|
+|400-300-150-200|1 |
+
+#### Dicas Importantes:
+
+- A entrada e a saída já são tratadas no arquivo fornecido para ler e imprimir os dados no formato esperado pela questão. Vocês devem APENAS implementar a função solicitada no problema
+- Não use arquivos .h (coloque todas as definições de tipo no arquivo .c)
+- Veja outras dicas em http://www.ic.uff.br/~vanessa/courses/runcodes.html
---
+++ Trabalhos de Implementacao/01 - Arvore Zigue-Zague/ziguezague.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct sNoA {
+    int chave;
+    struct sNoA *esq;
+    struct sNoA *dir;
+} TNoA;
+
+int ehZigueZague(TNoA *raiz) {
+    TNoA *no_atual = raiz;
+    while (no_atual != NULL) {
+        if (no_atual->esq != NULL && no_atual->dir != NULL) return 0;
+        if (no_atual->esq != NULL) no_atual = no_atual->esq;
+        else no_atual = no_atual->dir;
+    }
+    return 1;
+}
+
+void imprime(TNoA *nodo, int tab) {
+    for (int i = 0; i < tab; i++) {
+        printf("-");
+    }
+    if (nodo != NULL) {
+        printf("%d\n", nodo->chave);
+        imprime(nodo->esq, tab + 2);
+        printf("\n");
+        imprime(nodo->dir, tab + 2);
+    } else printf("vazio");
+}
+
+TNoA *insere(TNoA *no, int chave) {
+    if (no == NULL) {
+        no = (TNoA *) malloc(sizeof(TNoA));
+        no->chave = chave;
+        no->esq = NULL;
+        no->dir = NULL;
+    } else if (chave < (no->chave))
+        no->esq = insere(no->esq, chave);
+    else if (chave > (no->chave)) {
+        no->dir = insere(no->dir, chave);
+    } else {
+        printf("Inserção inválida! ");
+        exit(1);
+    }
+    return no;
+}
+
+int main(void) {
+
+    /* A função main lê os dados de entrada, cria a árvore e chama a função solicitada no problema
+     * depois imprime o resultado solicitado
+     * ELA NÃO DEVE SER MODIFICADA
+     * */
+    TNoA *raiz;
+    raiz = NULL;
+
+    char l[100];
+    char delimitador[] = "-";
+    char *ptr;
+    int valor;
+
+    /* lê valores para criar a arvore
+     * valores devem ser informados separados por traço
+     * exemplo: 1-3-5-2-7-9-21-6 */
+    scanf("%s", l);
+    //quebra a string de entrada
+    ptr = strtok(l, delimitador);
+    while(ptr != NULL) {
+        valor = atoi(ptr);
+        raiz = insere(raiz, valor);
+        ptr = strtok(NULL, delimitador);
+    }
+
+    //Chama função
+    printf("%d", ehZigueZague(raiz));
+}
