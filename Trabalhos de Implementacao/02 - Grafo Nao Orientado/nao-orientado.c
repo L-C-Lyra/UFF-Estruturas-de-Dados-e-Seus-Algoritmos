1c9500d78ca9c9d4229ee25231406e1138b0f154
Lucas C Lyra Pereira <148347259+L-C-Lyra@users.noreply.github.com>
2026-04-21 10:11:03 -0300

    :sparkles: feat: "Trabalhos de Implementacao/02 - Grafo Nao Orientado"

---
+++ Trabalhos de Implementacao/02 - Grafo Nao Orientado/nao-orientado.c
@@ -0,0 +1,138 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct vizinho {
+    int id_vizinho;
+    int peso;
+    struct vizinho *prox;
+}TVizinho;
+
+typedef struct grafo{
+    int id_vertice;
+    int cor;
+    TVizinho *prim_vizinho;
+    struct grafo *prox;
+}TGrafo;
+
+TGrafo *insere_vertice(TGrafo *g, int id) {
+    TGrafo *vertice = (TGrafo *) malloc(sizeof(TGrafo));
+    vertice->id_vertice = id;
+    vertice->cor = -1;
+    vertice->prox = g;
+    vertice->prim_vizinho = NULL;
+    return vertice;
+}
+
+void libera_vizinho(TVizinho *vizinho) {
+    if (vizinho != NULL) {
+        libera_vizinho(vizinho->prox);
+        free(vizinho);
+    }
+}
+
+void libera_vertice(TGrafo *vertice) {
+    if (vertice != NULL) {
+        libera_vizinho(vertice->prim_vizinho);
+        libera_vertice(vertice->prox);
+        free(vertice);
+    }
+}
+
+TGrafo *busca_vertice(TGrafo *vertice, int id) {
+    while ((vertice != NULL) && (vertice->id_vertice != id)) {
+        vertice = vertice->prox;
+    }
+    return vertice;
+}
+
+TVizinho *busca_vizinho(TVizinho *vizinho, int id) {
+    while ((vizinho != NULL) && (vizinho->id_vizinho != id)) {
+        vizinho = vizinho->prox;
+    }
+    return vizinho;
+}
+
+void insere_aresta(TGrafo *g, int origem, int destino, int peso) {
+    TGrafo *vertice = busca_vertice(g, origem);
+    TVizinho *vizinho = (TVizinho *) malloc(sizeof(TVizinho));
+    vizinho->id_vizinho = destino;
+    vizinho->peso = peso;
+    vizinho->prox = vertice->prim_vizinho;
+    vertice->prim_vizinho = vizinho;
+}
+
+void imprime(TGrafo *vertice) {
+    while (vertice != NULL) {
+        printf("Vertice: %d\n", vertice->id_vertice);
+        printf("Vizinhos: ");
+        TVizinho *vizinho = vertice->prim_vizinho;
+        while (vizinho != NULL) {
+            printf("%d ", vizinho->id_vizinho);
+            vizinho = vizinho->prox;
+        }
+        printf("\n\n");
+        vertice = vertice->prox;
+    }
+}
+
+int ehNaoOrientado(TGrafo *g) {
+    TGrafo *vertice_atual = g;
+    while (vertice_atual != NULL) {
+        int v1 = vertice_atual->id_vertice;
+        TVizinho *vizinho_atual = vertice_atual->prim_vizinho;
+        while (vizinho_atual != NULL) {
+            int v2 = vizinho_atual->id_vizinho;
+            int peso_ida = vizinho_atual->peso;
+            TGrafo *vertice_proximo = busca_vertice(g, v2);
+            if (vertice_proximo == NULL) return 0;
+            TVizinho *aresta_volta = busca_vizinho(vertice_proximo->prim_vizinho, v1);
+            if ((aresta_volta == NULL) || (aresta_volta->peso != peso_ida)) return 0;
+            vizinho_atual = vizinho_atual->prox;
+        }
+        vertice_atual = vertice_atual->prox;
+    }
+    return 1;
+}
+
+int main() {
+    /* A função main lê os dados de entrada, cria o grafo e chama a função solicitada no problema
+    * depois imprime o resultado
+    * ELA NÃO DEVE SER MODIFICADA
+    * */
+    int num_vertices, num_arestas;
+    int id;
+    int origem, destino, peso;
+    char l[100];
+    char delimitador[] = "-";
+    char *ptr;
+    int i;
+    TGrafo *g = NULL;
+
+    //le numero de vertices
+    scanf("%d", &num_vertices);
+    //le e cria os vertices
+    for (i = 0; i < num_vertices; i++) {
+        scanf("%s", l);
+        id = atoi(l);
+        g = insere_vertice(g, id);
+    }
+
+    //Le numero de arestas e depois le os dados de cada aresta
+    //Cria as arestas no grafo
+    scanf("%d", &num_arestas);
+    for (i = 0; i < num_arestas; i++) {
+        scanf("%s", l);
+        //quebra a string de entrada
+        ptr = strtok(l, delimitador);
+        origem = atoi(ptr);
+        ptr = strtok(NULL, delimitador);
+        destino = atoi(ptr);
+        ptr = strtok(NULL, delimitador);
+        peso = atoi(ptr);
+        insere_aresta(g, origem, destino, peso);
+    }
+
+    printf("%d", ehNaoOrientado(g));
+    libera_vertice(g);
+}
